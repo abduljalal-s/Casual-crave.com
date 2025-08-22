e9754e38335e73d1c3b226461a76f5691f4bb05c
abduljalal-s <nightmadecoder@gmail.com>
2025-08-22 14:41:59 +0100

    update11

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // swcMinify: true,
-  output: 'standalone', // Recommended for Cloudflare Workers or external deployments
-
-  // Only add experimental if you actually need server actions
-  // experimental: {
-  //   serverActions: {}, // valid object if needed
-  // },
+module.exports = {
+	async redirects() {
+		return [];
+	},
 };
-
-module.exports = nextConfig;
